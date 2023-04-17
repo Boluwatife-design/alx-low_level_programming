abf191dae2a121ddb98a5766fc26250b5365c8a5
temitope <temitopeolowoniyi12@gmail.com>
2023-04-17 15:06:47 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,8 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
+	if (d->age == NULL)
+		d->age = "(nil)";
 
 	printf("Name: %s\nAge: %f\nowner: %s\n", d->name, d->age, d->owner);
 }
