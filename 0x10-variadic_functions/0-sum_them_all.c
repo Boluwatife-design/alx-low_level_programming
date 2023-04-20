4b7881b2472d885e5272308b8f0f996048d3b6ee
temitope <temitopeolowoniyi12@gmail.com>
2023-04-20 05:30:21 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,8 +2,7 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all-function that returns
- * 	sum of all parameters
+ * sum_them_all-function that returns parameters
  * @n: integer
  * @...: variadic ellipses
  * Return: 0 if n == 0
@@ -13,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i, sum = 0;
 
-	va_start(ap,n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
