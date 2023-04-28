f917452b11f247594fba257c79cf27dffea3b26e
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-04-28 10:05:44 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void first(void)_attribute_((constructor));
+/**
+ * first-prints a sentence before main
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
