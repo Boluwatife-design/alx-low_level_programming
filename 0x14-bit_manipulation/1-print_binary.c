12d604a8334cc1a9f0953ff3647edc6ab184b72a
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-07-08 09:43:21 -0700

    main.h

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i,count = 0;
-	unsigned long int value;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 63; i >= 0; i--)
-	{
-		value = n >> i;
-
-		if (value & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
-		_putchar('0');
+	putchar((n & 1) + '0');
 }
